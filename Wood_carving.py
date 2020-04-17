5076ad0eb64da3c8fc805e89ca602b40340b04fc
ErigoLee <57980181+ErigoLee@users.noreply.github.com>
2020-04-18 01:57:00 +0900

    Create  Wood_carving.py

---
+++ Wood_carving.py
@@ -0,0 +1,25 @@
+##funtion
+def sort_work(arr):
+    arr_len = len(arr)
+    i=0
+    while i < arr_len-1:
+        if arr[i]>arr[i+1]:
+            temp = arr[i]
+            arr[i] = arr[i+1]
+            arr[i+1] = temp
+            for j in arr:
+                print(j,end=" ")
+            print()
+        i=i+1
+
+    index = 0
+    for i in range(0,arr_len-1,1):
+        if arr[i]>arr[i+1]:
+            index=1
+
+    if index==1:
+        sort_work(arr)
+
+##main
+arr = list(map(int,input().split()))
+sort_work(arr)
