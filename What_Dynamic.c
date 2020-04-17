92438fe8ec452e80251151adf3715647bdebc465
ErigoLee <57980181+ErigoLee@users.noreply.github.com>
2020-04-18 01:21:59 +0900

    Create What_Dynamic.c

---
+++ What_Dynamic.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+//#include <stdlib.h>
+const int mod = 1e9 + 7;
+long long int memo[1001][1001] = { 0 };
+int main() {
+
+    int num, num2;
+    scanf("%d", &num);
+    scanf("%d", &num2);
+   
+    memo[1][1] = 1;
+
+    for (int i = 0; i <= num; i++) {
+        for (int j = 0; j <= num2; j++) {
+            if (i * j > 1)
+                memo[i][j] = ((memo[i - 1][j]) + (memo[i][j - 1]) + (memo[i - 1][j - 1]))%mod;
+        }
+    }
+
+
+    printf("%lld\n", memo[num][num2]);
+    return 0;
+}
