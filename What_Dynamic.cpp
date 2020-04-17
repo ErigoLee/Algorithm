75cf3948755b5ed9900b2c4caab4a83664672ca7
ErigoLee <57980181+ErigoLee@users.noreply.github.com>
2020-04-18 01:22:39 +0900

    Create What_Dynamic.cpp

---
+++ What_Dynamic.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+long long memo[1001][1001] = { 0 };
+int main() {
+    const int mod = 1000000007;
+    
+
+    int num, num2;
+    //int answer;
+    cin >> num >> num2;
+    memo[1][1] = 1;
+
+    for (int i = 0; i <= num; i++) {
+        for (int j = 0; j <= num2; j++) {
+            if (i * j > 1)
+                memo[i][j] = ((memo[i - 1][j]) + (memo[i][j - 1]) + (memo[i - 1][j - 1]))%mod;
+        }
+    }
+
+    cout << memo[num][num2] << endl;
+
+    return 0;
+}
