d407dab08de93fc92ca7d77a16caea3d5afcb4a6
ErigoLee <57980181+ErigoLee@users.noreply.github.com>
2021-07-22 17:41:31 +0900

    10871

--- PythonApplication1.py
+++ PythonApplication1.py
@@ -1,11 +1,14 @@
-while(True):
-    s, b = map(int, input().split())
-    if 0 < s and s <= 10000:
-        if 0 < b and b <= 10000:
+num_2=list()
+while True:
+    N, X = map(int,input().split())
+    if 1<= N and N <= 10000:
+        if 1<= X and X <= 10000:
             break
 
-print(s+b)
-print(s-b)
-print(s*b)
-print(s//b)
-print(s%b)
+num = list(map(int,input().split()))
+for i in num:
+    if i < X:
+        num_2.append(i)
+
+for i in num_2:
+    print(i,end=" ")
