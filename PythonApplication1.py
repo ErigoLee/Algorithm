d7700d948d7d79f5c0df3306d018f42eb1c901c6
ErigoLee <57980181+ErigoLee@users.noreply.github.com>
2021-07-22 13:45:07 +0900

    1000

--- PythonApplication1.py
+++ PythonApplication1.py
@@ -1,6 +1,7 @@
-##main
-arr=list(map(int,input()))
-arr.sort()
-arr.reverse()
-for i in arr:
-    print(i,end="")
+while(True):
+    s, b = map(int, input().split())
+    if 0 < s and s < 10:
+        if 0 < b and b < 10:
+            break
+
+print(s+b)
