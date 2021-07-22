4eb66e193c10bf80963f3a22628dd7f1926f81ee
ErigoLee <57980181+ErigoLee@users.noreply.github.com>
2021-07-22 16:35:11 +0900

    4485

--- 소스.cpp
+++ 소스.cpp
@@ -1,65 +1,88 @@
 #include <iostream>
 #include <vector>
-#include <algorithm>
+#include <queue>
+
+#define INF 999999999;
 
 using namespace std;
-int bottom[100001];
-int up[100001];
 
-//binary search
-int BSearch(int arr[], int len, int target) {
+int dijkstra(int n, vector<vector<int>> test_case)
+{
+    queue<pair<int, int>> qu;
+    int shortest[125][125];
+    int cal[4][2] = { {1,0},{-1,0},{0,1},{0,-1} };
+    for (int i = 0; i < n; i++) for (int j = 0; j < n; j++) shortest[i][j] = INF;
 
-    int first = 0;
-    int last = len - 1;
-    int mid;
-    int ans = 0;
-    while (first <= last)
+    shortest[0][0] = test_case[0][0];
+
+    pair<int, int> p = make_pair(0, 0);
+    qu.push(p);
+
+    while (!qu.empty())
     {
-        mid = (first + last) / 2;
-        if (target > arr[mid]) {
-            ans = mid + 1;
-            first = mid + 1;
-        }
-        else
-            last = mid - 1;
+        int first = qu.front().first;
+        int second = qu.front().second;
 
+        qu.pop();
+        for (int i = 0; i < 4; i++)
+        {
+            int next_first = first + cal[i][0];
+            int next_second = second + cal[i][1];
+            if ((next_first >= 0 && next_first < n) && (next_second >= 0 && next_second < n))
+            {
+                if (shortest[next_first][next_second] > shortest[first][second] + test_case[next_first][next_second])
+                {
+                    shortest[next_first][next_second] = shortest[first][second] + test_case[next_first][next_second];
+                    pair<int, int> next = make_pair(next_first, next_second);
+                    qu.push(next);
+                }
+            }
+        }
     }
-    return ans;
+
+    int answer = shortest[n - 1][n - 1];
+    return answer;
 }
+
 
 
 int main() {
 
-    int count, level;
-    int min = 1000000000;
-    int cnt = 0;
-    cin >> count >> level;
+    int count;
+    vector<vector<int>> test_case;
+    vector<int> result;
 
-    for (int i = 0; i < count / 2; i++)
+    while (true)
     {
-        cin >> bottom[i] >> up[i];
-    }
+        cin >> count;
+        if (count <= 0)
+            break;
 
-    sort(bottom, bottom + count / 2);
-    sort(up, up + count / 2);
+        for (int i = 0; i < count; i++)
+        {
+            vector<int> a;
+            for (int j = 0; j < count; j++)
+            {
+                int b;
+                cin >> b;
+                a.push_back(b);
+            }
+            test_case.push_back(a);
 
-
-    for (int i = 1; i <= level; i++)
-    {
-        int down_count = 0, up_count = 0;
-        int size = count / 2;
-        down_count = size - BSearch(bottom, size, i);
-        up_count = size - BSearch(up, size, level - i + 1);
-        if (min > down_count + up_count) {
-            min = down_count + up_count;
-            cnt = 0;
+            while (!a.empty()) a.pop_back();
         }
 
-        if (min == down_count + up_count)
-            cnt++;
+        int res = dijkstra(count, test_case);
+        result.push_back(res);
+        while (!test_case.empty()) test_case.pop_back();
     }
 
-    cout << min << " " << cnt;
+    int size = result.size();
+
+    for (int i = 0; i < size; i++)
+    {
+        cout << "Problem " << i + 1 << ": " << result[i] << endl;
+    }
 
     return 0;
 }
