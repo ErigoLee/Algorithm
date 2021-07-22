81d89862aea612c0c6b2a1f196f59089dc544a30
ErigoLee <57980181+ErigoLee@users.noreply.github.com>
2021-07-22 15:45:26 +0900

    2557

--- PythonApplication1.py
+++ PythonApplication1.py
@@ -1,8 +1,1 @@
-while True:
-    N = int(input())
-    if 1<= N and N <= 100:
-        break
-star='*'
-temp=' '
-for i in range(1,N+1,1):
-    print("%s%s"%(temp*(N-i),star*i))
+print("Hello World!")
