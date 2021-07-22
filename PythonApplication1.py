c02b55a8250efcd12550c3335fd560305ed14b96
ErigoLee <57980181+ErigoLee@users.noreply.github.com>
2021-07-22 17:55:48 +0900

    10998

--- PythonApplication1.py
+++ PythonApplication1.py
@@ -1,21 +1,7 @@
-import math
-import itertools
-N = 0
-T = []
-s = []
-
-##입력값 받기
 while(True):
-    N = int(input())
-    if 1<=N and N<=8:
-        break
+    s, b = map(int, input().split())
+    if 0 < s and s < 10:
+        if 0 < b and b < 10:
+            break
 
-factor_num = math.factorial(N)
-
-for i in range(1,N+1,1):
-    T.append(i)
-s =itertools.permutations(T)
-for k in s:
-    for j in range(0,N,1):
-        print(k[j],end=" ")
-    print()
+print(s*b)
