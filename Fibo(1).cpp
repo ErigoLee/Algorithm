1ce87be47685db3db51bf79c1e931b5888a63c9a
ErigoLee <57980181+ErigoLee@users.noreply.github.com>
2021-07-23 00:27:12 +0900

    Add files via upload

---
+++ Fibo(1).cpp
@@ -0,0 +1,17 @@
+//failed code
+#include <iostream>
+
+using namespace std;
+int main() {
+  int fibo[91];
+  int number;
+  cin>>number;
+
+  fibo[0]=0;
+  fibo[1]=1;
+  for(int i=2;i<=number;i++)
+    fibo[i] = fibo[i-1]+fibo[i-2];
+  
+  cout<<fibo[number]<<endl;
+  return 0;
+}
