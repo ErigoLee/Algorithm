90bd5f8161699f192a452e254370b92e1dce5511
ErigoLee <57980181+ErigoLee@users.noreply.github.com>
2021-07-22 17:50:15 +0900

    10972

--- PythonApplication1.py
+++ PythonApplication1.py
@@ -1,10 +1,39 @@
-sum=list()
-while True:
-    a, b = map(int,input().split())
-    if a==0 and b==0:
+##다음 순열을 찾을 수 있는 함수
+def next_permutation():
+    i = N-1
+    while i > 0 and T[i-1]>=T[i]:
+        i = i-1
+    if i == 0:
+        return False
+    j=N-1
+    while T[i-1] >= T[j]:
+        j = j - 1
+    T[i-1],T[j]=T[j],T[i-1]
+    j = N - 1
+    while i<j:
+        T[i],T[j] = T[j],T[i]
+        j = j-1
+        i = i+1
+    return True
+##입력값 받기
+while(True):
+    N = int(input())
+    if (1<=N and N<=10000):
         break
-    sum_num=a+b
-    sum.append(sum_num)
 
-for i in sum:
-    print(i)
+##순열값 받기
+while(True):
+    a = 0
+    input_number = map(int,input().split())
+    T = list(input_number)
+    for i in T:
+        if (i > N or i<=0):
+            a = a+1
+    if a == 0:
+        break
+S = next_permutation()
+if S:
+    for i in T:
+        print(i,end=" ")
+else:
+    print(-1)
