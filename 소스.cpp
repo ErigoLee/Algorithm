afe91840e5e45a18c40e53cc0158a4d68be44c2b
ErigoLee <57980181+ErigoLee@users.noreply.github.com>
2021-07-22 14:48:42 +0900

    2250

--- 소스.cpp
+++ 소스.cpp
@@ -1,54 +1,78 @@
 #include <iostream>
 #include <vector>
-#include <queue>
-#include <algorithm>
+#define INF 1000000000
 
 using namespace std;
 
+int line[10001];
+pair<int, int> node[10001];
+int high[10001];
+int low[10001];
+
+int index;
+
+void inorder(int num, int level)
+{
+
+    if (node[num].first > 0)
+        inorder(node[num].first, level + 1);
+
+    if (low[level] > index)
+        low[level] = index;
+
+    if (high[level] < index)
+        high[level] = index;
+
+    index++;
+
+    if (node[num].second > 0)
+        inorder(node[num].second, level + 1);
+}
+
 int main() {
-    queue <int> q;
-    vector <int> priority;
-    vector <int> result;
-    int count;  //È½¼ö
-    int amount; //°³¼ö
-    int index;
+
+    int count;
+
+    int root = 0;
+
     cin >> count;
-
-    for (int i = 0; i < count; i++) {
-        cin >> amount >> index;
-        for (int j = 0; j < amount; j++) {
-            int a;
-            cin >> a;
-            q.push(a);
-            priority.push_back(a);
-        }
-
-        int count2 = 0;
-        sort(priority.begin(), priority.end());
-        while (true) {
-            if (q.front() == priority.back()) {
-                count2++;
-                if (index == 0) {
-                    result.push_back(count2);
-                    break;
-                }
-                q.pop();
-                priority.pop_back();
-            }
-            else {
-                int num = q.front();
-                q.pop();
-                q.push(num);
-            }
-            if (index == 0) index = q.size() - 1;
-            else index--;
-        }
-        while (!q.empty()) q.pop();
-        priority.clear();
+    for (int i = 0; i < count; i++)
+    {
+        int a, b, c;
+        cin >> a >> b >> c;
+        node[a] = make_pair(b, c);
+        line[a]++;
+        if (b != -1)
+            line[b]++;
+        if (c != -1)
+            line[c]++;
     }
 
-    for (int i = 0; i < count; i++)
-        cout << result[i] << endl;
+    for (int i = 1; i <= count; i++) low[i] = INF;
 
+    for (int i = 1; i <= count; i++)
+    {
+        if (line[i] == 1)
+        {
+            root = i;
+            break;
+        }
+    }
+    index = 1;
+    inorder(root, 1);
+    int max_level = 0;
+    int max = 0;
+
+    for (int i = 1; i <= count; i++)
+    {
+        int width = high[i] - low[i] + 1;
+        if (width > max)
+        {
+            max = width;
+            max_level = i;
+        }
+    }
+
+    cout << max_level << " " << max << endl;
     return 0;
 }
