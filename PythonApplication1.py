6e76b059b47e89cee952ef31dbc51d8a4b476e84
ErigoLee <57980181+ErigoLee@users.noreply.github.com>
2021-07-22 14:37:23 +0900

    2178

--- PythonApplication1.py
+++ PythonApplication1.py
@@ -1,63 +1,45 @@
-N = 0
-root = []
-left = []
-right = []
-str_front =[]
-str_middle=[]
-str_back=[]
+##main value
+graph=list()
+N=0 ##first
+M=0 ##second
+##dx[0],dy[0] =>오른쪽
+##dx[1],dy[1] =>왼쪽
+##dx[2],dy[2] =>아래
+##dx[3],dy[3] =>위
 
-##(root)(left)(right)
-def front_recursive(index):
-    if root[index]=='.':
-        return
-    str_front.append(root[index])
-    front_recursive(root.index(left[index])) ##left
-    front_recursive(root.index(right[index])) ##right
+dx = [0,0,1,-1]
+dy = [1,-1,0,0]
+
+## bfs function
+def bfs(start_node_1,start_node_2):
+    visit = list()##방문한 노드
+    queue = list()##BFS로 사용할 큐
+    distance = [[0 for _ in range(M)] for _ in range(N)]
+    distance[0][0] = 1
+
+    queue.append([start_node_1,start_node_2])
+    while queue:
+        [start_node_1,start_node_2]=queue.pop(0)
+        if [start_node_1,start_node_2] is not visit:
+            visit.append([start_node_1,start_node_2])
+            for i in range(4):
+                node_1 = start_node_1+dx[i]
+                node_2 = start_node_2+dy[i]
+                if node_1>=0 and node_1<N and node_2>=0 and node_2<M and graph[node_1][node_2]==1:
+                    if [node_1,node_2] not in visit and [node_1,node_2] not in queue:
+                        queue.append([node_1,node_2])
+                        distance[node_1][node_2] = distance[start_node_1][start_node_2]+1
+    return distance[N-1][M-1]
 
 
-##(left)(root)(right)
-def middle_recursive(index):
-    if root[index] == '.':
-        return
-    middle_recursive(root.index(left[index]))  ##left
-    str_middle.append(root[index])
-    middle_recursive(root.index(right[index]))  ##right
-
-##(left)(right)(root)
-def back_recursive(index):
-    if root[index]=='.':
-        return
-    back_recursive(root.index(left[index])) ##left
-    back_recursive(root.index(right[index])) ##right
-    str_back.append(root[index])
-
-##main##
+##main input
 while True:
-    N = int(input())
-    if 1 <= N and N <= 26:
+    N, M = map(int, input().split())
+    if 2<=N and N<=100 and 2<=M and M<=100:
         break
 
-##input
-for i in range(0, N, 1):
-    A, B, C = input().split()
-    root.append(A)
-    left.append(B)
-    right.append(C)
-root.append('.')
-left.append('.')
-right.append('.')
+for i in range(N):
+    graph.append(list(map(int, str(input()))))
 
-##front_recursive
-front_recursive(0)
-for i in str_front:
-    print(i,end="")
-print()
-##middle_recursive
-middle_recursive(0)
-for i in str_middle:
-    print(i,end="")
-print()
-##back_recursive
-back_recursive(0)
-for i in str_back:
-    print(i,end="")
+visit = bfs(0,0)
+print(visit)
