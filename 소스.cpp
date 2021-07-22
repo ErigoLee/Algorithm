2e7e61aa892d5142eec91fcf9d266ad376e2f082
ErigoLee <57980181+ErigoLee@users.noreply.github.com>
2021-07-22 16:37:22 +0900

    4485-1

--- 소스.cpp
+++ 소스.cpp
@@ -1,88 +1,72 @@
 #include <iostream>
 #include <vector>
 #include <queue>
-
-#define INF 999999999;
-
 using namespace std;
 
-int dijkstra(int n, vector<vector<int>> test_case)
+struct Pos
 {
-    queue<pair<int, int>> qu;
-    int shortest[125][125];
-    int cal[4][2] = { {1,0},{-1,0},{0,1},{0,-1} };
-    for (int i = 0; i < n; i++) for (int j = 0; j < n; j++) shortest[i][j] = INF;
-
-    shortest[0][0] = test_case[0][0];
-
-    pair<int, int> p = make_pair(0, 0);
-    qu.push(p);
-
-    while (!qu.empty())
-    {
-        int first = qu.front().first;
-        int second = qu.front().second;
-
-        qu.pop();
-        for (int i = 0; i < 4; i++)
-        {
-            int next_first = first + cal[i][0];
-            int next_second = second + cal[i][1];
-            if ((next_first >= 0 && next_first < n) && (next_second >= 0 && next_second < n))
-            {
-                if (shortest[next_first][next_second] > shortest[first][second] + test_case[next_first][next_second])
-                {
-                    shortest[next_first][next_second] = shortest[first][second] + test_case[next_first][next_second];
-                    pair<int, int> next = make_pair(next_first, next_second);
-                    qu.push(next);
-                }
-            }
-        }
-    }
-
-    int answer = shortest[n - 1][n - 1];
-    return answer;
-}
-
-
+    int x;
+    int y;
+};
 
 int main() {
+    int test_case, count = 1;
+    vector<int> results;
+    cin >> test_case;
 
-    int count;
-    vector<vector<int>> test_case;
-    vector<int> result;
-
-    while (true)
+    while (test_case != 0)
     {
-        cin >> count;
-        if (count <= 0)
-            break;
-
-        for (int i = 0; i < count; i++)
+        int n = test_case;
+        int cave[125][125], obstacles[125][125];
+        for (int i = 0; i < n; i++)
         {
-            vector<int> a;
-            for (int j = 0; j < count; j++)
+            for (int j = 0; j < n; j++)
             {
-                int b;
-                cin >> b;
-                a.push_back(b);
+                cin >> cave[i][j];
+                obstacles[i][j] = 999999;
             }
-            test_case.push_back(a);
-
-            while (!a.empty()) a.pop_back();
         }
+        obstacles[0][0] = cave[0][0];
 
-        int res = dijkstra(count, test_case);
-        result.push_back(res);
-        while (!test_case.empty()) test_case.pop_back();
+        int rx[4] = { 1, 0, -1, 0 };
+        int ry[4] = { 0, 1, 0, -1 };
+
+        queue <Pos> q;
+        Pos a = { 0, 0 };
+        q.push(a);
+        while (!q.empty())
+        {
+            Pos npos = q.front();
+            q.pop();
+
+            for (int i = 0; i < 4; i++)
+            {
+                int nx = npos.x + rx[i];
+                int ny = npos.y + ry[i];
+                if (nx < 0 || ny < 0 || nx >= n || ny >= n)
+                    continue;
+
+                if (obstacles[nx][ny] > obstacles[npos.x][npos.y] + cave[nx][ny])
+                {
+                    obstacles[nx][ny] = obstacles[npos.x][npos.y] + cave[nx][ny];
+                    Pos nnpos = { nx, ny };
+                    q.push(nnpos);
+                }
+
+            }
+        }
+        results.push_back(obstacles[n - 1][n - 1]);
+
+        cin >> test_case;
     }
 
-    int size = result.size();
+    int size = results.size();
 
     for (int i = 0; i < size; i++)
     {
-        cout << "Problem " << i + 1 << ": " << result[i] << endl;
+        cout << "Problem " << i + 1 << ": " << results[i] << endl;
     }
+
 
     return 0;
 }
