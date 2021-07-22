a2bf88bd5813b963dc2d0f585ffb6e8a0bee717f
ErigoLee <57980181+ErigoLee@users.noreply.github.com>
2021-07-22 16:15:15 +0900

    2798

--- 소스.cpp
+++ 소스.cpp
@@ -1,16 +1,44 @@
 #include <iostream>
-
+#include <vector>
+#include <algorithm>
 using namespace std;
+
+int card_count[100];
 int main() {
-    long long fibo[91];
-    int number;
-    cin >> number;
+    int N, M;
+    int result = 0;
+    int gap = 100000;
+    cin >> N >> M;
+    vector<int>Com;
 
-    fibo[0] = 0;
-    fibo[1] = 1;
-    for (int i = 2; i <= number; i++)
-        fibo[i] = fibo[i - 1] + fibo[i - 2];
+    for (int i = 0; i < N; i++) {
+        cin >> card_count[i];
+        if (i < 3)
+            Com.push_back(1);
+        else
+            Com.push_back(0);
+    }
 
-    cout << fibo[number] << endl;
+    sort(Com.begin(), Com.end());
+
+    do {
+        int max = 0;
+        int gap2;
+        for (int i = 0; i < N; i++) {
+            if (Com[i] == 1)
+                max += card_count[i];
+        }
+        if (max <= M) {
+            gap2 = M - max;
+            if (gap2 < gap) {
+                result = max;
+                gap = gap2;
+            }
+        }
+
+    } while (next_permutation(Com.begin(), Com.end()));
+
+    cout << result << endl;
+
     return 0;
 }
