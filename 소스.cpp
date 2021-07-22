f182fc4129afacdb5591f4e0d93bc15f7fd4aab0
ErigoLee <57980181+ErigoLee@users.noreply.github.com>
2021-07-23 00:12:23 +0900

    14503

--- 소스.cpp
+++ 소스.cpp
@@ -2,123 +2,80 @@
 #include <vector>
 #include <queue>
 using namespace std;
-int n, m;
-int count_max;
-vector<pair<int, int>> virus;
+bool visited[50][50];
 
-void DFS(vector<vector<int>> lab)
-{
-
-    bool visited[9][9];
-    for (int i = 0; i < 9; i++) for (int j = 0; j < 9; j++) visited[i][j] = false;
-    int count = 0;
-
-    int dx[4] = { 1, 0, -1, 0 };
-    int dy[4] = { 0, 1, 0, -1 };
-    vector<pair<int, int>> virus_re;
-    //queue<pair<int, int>> virus_re;
-    for (int i = 0; i < virus.size(); i++)
-        virus_re.push_back({ virus[i].first,virus[i].second });
-    //virus_re.push({ virus[i].first,virus[i].second });
-
-
-    while (!virus_re.empty())
-    {
-        //int first = virus_re.front().first;
-        //int second = virus_re.front().second;
-        int first = virus_re.back().first;
-        int second = virus_re.back().second;
-
-        //virus_re.pop();
-        virus_re.pop_back();
-
-        for (int i = 0; i < 4; i++)
-        {
-            int next_first = first + dx[i];
-            int next_second = second + dy[i];
-
-            if (next_first < 0 || next_first >= n || next_second < 0 || next_second >= m)
-                continue;
-
-            if (lab[next_first][next_second] != 0 || visited[next_first][next_second] == true)
-                continue;
-
-            visited[next_first][next_second] = true;
-            lab[next_first][next_second] = 2;
-            //virus_re.push({ next_first,next_second });
-            virus_re.push_back({ next_first,next_second });
-
-        }
-
-
-    }
-
-    for (int i = 0; i < n; i++)
-    {
-        for (int j = 0; j < m; j++)
-        {
-            if (lab[i][j] == 0)
-                count++;
-        }
-    }
-
-    if (count_max < count)
-        count_max = count;
-
-}
-
-void search(vector<vector<int>> lab, int cnt)
-{
-    if (cnt == 3)
-    {
-        DFS(lab);
-        return;
-    }
-    else
-    {
-        for (int i = 0; i < n; i++)
-        {
-            for (int j = 0; j < m; j++)
-            {
-                if (lab[i][j] == 0)
-                {
-                    lab[i][j] = 1;
-                    cnt++;
-                    search(lab, cnt);
-                    cnt--;
-                    lab[i][j] = 0;
-                }
-            }
-        }
-    }
-}
 
 int main() {
 
-    vector<vector<int>> lab;
+    vector<vector<int>> route;
+    int d_x[4] = { 0,-1,0,1 };
+    int d_y[4] = { -1,0,1,0 };
+    int back_x[4] = { 1,0,-1,0 };
+    int back_y[4] = { 0,-1,0,1 };
+    int n, m;
+    int x, y, direction;
+    int result = 0;
 
     cin >> n >> m;
+    cin >> x >> y >> direction;
 
     for (int i = 0; i < n; i++)
     {
         vector<int> a;
         for (int j = 0; j < m; j++)
         {
-            int b;
-            cin >> b;
-            a.push_back(b);
-            if (b == 2)
-                virus.push_back({ i,j });
-
+            int temp;
+            cin >> temp;
+            a.push_back(temp);
         }
-        lab.push_back(a);
+        route.push_back(a);
         a.clear();
     }
 
 
-    search(lab, 0);
+    queue<pair<int, int>> qu;
+    qu.push({ x,y });
+    visited[x][y] = true;
+    result++;
+    while (!qu.empty())
+    {
+        int node_x = qu.front().first;
+        int node_y = qu.front().second;
+        qu.pop();
 
-    cout << count_max << endl;
+        for (int i = 0; i < 4; i++)
+        {
+            int next_x = node_x + d_x[direction];
+            int next_y = node_y + d_y[direction];
+
+            if (route[next_x][next_y] == 0 && visited[next_x][next_y] == false)
+            {
+                visited[next_x][next_y] = true;
+                result++;
+                qu.push({ next_x,next_y });
+                direction = direction - 1;
+                if (direction == -1)
+                    direction = 3;
+                break;
+            }
+
+            direction = direction - 1;
+            if (direction == -1)
+                direction = 3;
+        }
+
+        if (qu.empty())
+        {
+            int next_x = node_x + back_x[direction];
+            int next_y = node_y + back_y[direction];
+            if (route[next_x][next_y] == 0)
+                qu.push({ next_x,next_y });
+        }
+
+    }
+
+
+    cout << result << endl;
 
     return 0;
 }
