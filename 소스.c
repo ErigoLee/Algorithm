46038a7d37487d709007a3c81365f01499953fac
ErigoLee <57980181+ErigoLee@users.noreply.github.com>
2021-07-22 16:39:42 +0900

    5585

--- 소스.c
+++ 소스.c
@@ -1,68 +1,22 @@
 #include <stdio.h>
-#include <math.h>
 
-int count_function(int N, int r, int c) {
+int main(void) {
+    int coin[6] = { 500, 100, 50, 10, 5, 1 };
+    int value;
+    int coin_count = 0;
+    int charge;
 
-	int count = 0; //횟수 세기
-	if (N == 1) {
-		if (r == 0 && c == 0) {
-			return count;
-		}
-		else
-			count++;
-		if (r == 0 && c == 1) {
-			return count;
-		}
-		else
-			count++;
-		if (r == 1 && c == 0) {
-			return count;
-		}
-		else
-			count++;
-		if (r == 1 && c == 1) {
-			return count;
-		}
-		else
-			count++;
-	}
-	else {
-		int index_N = N - 1;
-		int index = (int)pow(2, index_N);
-		if (r < index && c < index) {
-			return count_function(N - 1, r, c);
-		}
-		else if (r < index && c >= index)
-			return index * index + count_function(N - 1, r, c - index);
-		else if (r >= index && c < index)
-			return 2 * index * index + count_function(N - 1, r - index, c);
-		else
-			return 3 * index * index + count_function(N - 1, r - index, c - index);
-	}
+    scanf("%d", &value);
 
-}
+    charge = 1000 - value;
 
-int main(int argc, char** argv) {
+    for (int i = 0; i < 6; i++)
+    {
+        int num = charge / coin[i];
+        coin_count += num;
+        charge = charge % coin[i];
+    }
 
-	int N; // 2^N에서의 N의 import 값 저장할 변수
-	long int index; // 2^N의 value 저장할 변수
-	int r; //(r,c)에서 r value 저장할 변수
-	int c; //(r,c)에서 s value 저장할 변수
-
-
-	while (1) {
-		scanf("%d", &N);
-		index = pow(2, N);
-		scanf("%d", &r);
-		scanf("%d", &c);
-
-		if (1 <= N && N <= 15)
-			if (0 <= r && r <= index - 1)
-				if (0 <= c && c <= index - 1)
-					break;
-	}
-	int count = count_function(N, r, c);
-	printf("%d", count);
-
-	return 0;
+    printf("%d\n", coin_count);
+    return 0;
 }
