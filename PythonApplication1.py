2ef6591fc82521319eba18b513febde608cd3d3e
ErigoLee <57980181+ErigoLee@users.noreply.github.com>
2021-07-22 14:03:32 +0900

    1110

--- PythonApplication1.py
+++ PythonApplication1.py
@@ -1,7 +1,16 @@
-while(True):
-    s, b = map(int, input().split())
-    if 0 < s and s < 10:
-        if 0 < b and b < 10:
-            break
-
-print(s/b)
+num = int(input())
+num2=num
+b = 1
+while True:
+    if num2 < 10:
+        t = 0
+        s = num2
+    else:
+        t = num2//10
+        s = num2%10
+    temp = (t+s)%10
+    num2 = s*10 + temp
+    if num2 == num:
+        break
+    b = b+1
+print(b)
