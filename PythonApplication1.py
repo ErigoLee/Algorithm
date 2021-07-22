0354a964edc74758204cb24c2efa51066e690188
ErigoLee <57980181+ErigoLee@users.noreply.github.com>
2021-07-22 15:48:37 +0900

    2562

--- PythonApplication1.py
+++ PythonApplication1.py
@@ -1,1 +1,18 @@
-print("Hello World!")
+a = 0
+b = 0
+temp = 0
+max = 0
+num = list()
+while True:
+    try:
+        n = int(input())
+        num.append(n)
+        if max < n:
+            max = n
+            b = a+1
+        a = a + 1
+    except:
+        break
+num.sort()
+print(num[len(num)-1])
+print(b)
