ae20c6993ca274f88c52d135ff612462b972d046
ErigoLee <57980181+ErigoLee@users.noreply.github.com>
2021-07-22 17:29:07 +0900

    10430

--- PythonApplication1.py
+++ PythonApplication1.py
@@ -1,5 +1,14 @@
-print("|\_/|")
-print("|q p|   /}")
-print('( 0 )"""\ ')
-print('|"^"`    |')
-print("||_/=\\\\__| ")
+while(True):
+    s, b, t = map(int, input().split())
+    if 2 <= s and s <= 10000:
+        if 2 <= b and b <= 10000:
+            if 2 <= t and t <= 10000:
+                break
+M = (s+b)%t
+print(M)
+N = (s%t + b%t)%t
+print(N)
+L = (s*b)%t
+print(L)
+K = (s%t * b%t)%t
+print(K)
