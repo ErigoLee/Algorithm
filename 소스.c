eb157337083207fbf84429c64c1ec58d84913470
ErigoLee <57980181+ErigoLee@users.noreply.github.com>
2021-07-22 16:43:31 +0900

    5639

--- 소스.c
+++ 소스.c
@@ -1,22 +1,95 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main(void) {
-    int coin[6] = { 500, 100, 50, 10, 5, 1 };
-    int value;
-    int coin_count = 0;
-    int charge;
+typedef struct node_t
+{
+    int num;
+    struct node_t* left;
+    struct node_t* right;
+} Node;
 
-    scanf("%d", &value);
+Node* head = NULL;
 
-    charge = 1000 - value;
+Node* create_node(int num)
+{
+    Node* new_Node = (Node*)malloc(sizeof(Node));
 
-    for (int i = 0; i < 6; i++)
+    new_Node->num = num;
+    new_Node->left = NULL;
+    new_Node->right = NULL;
+
+    return new_Node;
+}
+void left_connect(Node* node, int num);
+void right_connect(Node* node, int num);
+
+void left_connect(Node* node, int num)
+{
+    if (node->left == NULL)
+        node->left = create_node(num);
+    else
     {
-        int num = charge / coin[i];
-        coin_count += num;
-        charge = charge % coin[i];
+        if (node->left->num > num)
+            left_connect(node->left, num);
+        else
+            right_connect(node->left, num);
     }
 
-    printf("%d\n", coin_count);
+}
+
+void right_connect(Node* node, int num)
+{
+    if (node->right == NULL)
+        node->right = create_node(num);
+    else
+    {
+        if (node->right->num > num)
+            left_connect(node->right, num);
+        else
+            right_connect(node->right, num);
+    }
+}
+
+void connect_node(int num)
+{
+    if (head == NULL)
+    {
+        head = create_node(num);
+    }
+    else
+    {
+        if (head->num > num)
+            left_connect(head, num);
+        else
+            right_connect(head, num);
+    }
+}
+
+void inorder(Node* node)
+{
+    if (node == NULL)
+        return;
+
+    inorder(node->left);
+    inorder(node->right);
+    printf("%d\n", node->num);
+
+}
+
+int main(void) {
+
+    int num;
+    //int a=0;
+    //scanf("%d", &num) !=EOF
+    while (scanf("%d", &num) != EOF)
+    {
+        //scanf("%d",&num);
+        connect_node(num);
+        //a++;
+    }
+
+
+    inorder(head);
+
     return 0;
 }
