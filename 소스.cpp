265422a85a443cfff2345b3ca03cb51e35e6f6f4
ErigoLee <57980181+ErigoLee@users.noreply.github.com>
2021-07-23 00:01:18 +0900

    14494

--- 소스.cpp
+++ 소스.cpp
@@ -1,37 +1,24 @@
 #include <iostream>
 
 using namespace std;
+long long memo[1001][1001] = { 0 };
+int main() {
+    const int mod = 1000000007;
 
-int title[1001];
-static int divide_num = 10007;
 
-int titling(int num)
-{
-    if (title[num])
-        return title[num];
+    int num, num2;
+    //int answer;
+    cin >> num >> num2;
+    memo[1][1] = 1;
 
-    if (num == 1)
-        return 1;
-    else if (num == 2)
-        return 3;
-    else {
-        int a = titling(num - 1) + 2 * titling(num - 2);
-        title[num] = a % divide_num;
-        return title[num];
+    for (int i = 0; i <= num; i++) {
+        for (int j = 0; j <= num2; j++) {
+            if (i * j > 1)
+                memo[i][j] = ((memo[i - 1][j] % mod) + (memo[i][j - 1] % mod) + (memo[i - 1][j - 1] % mod)) % mod;
+        }
     }
 
-}
-
-
-int main() {
-
-
-    int num;
-
-    cin >> num;
-
-    int answer = titling(num);
-    cout << answer << endl;
+    cout << memo[num][num2] % mod << endl;
 
     return 0;
 }
