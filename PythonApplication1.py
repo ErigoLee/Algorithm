c91f2806a6f33ae1c40be289f6ba07d3b13a0b32
ErigoLee <57980181+ErigoLee@users.noreply.github.com>
2021-07-22 17:22:28 +0900

    10171

--- PythonApplication1.py
+++ PythonApplication1.py
@@ -1,19 +1,4 @@
-##function
-def compare_find(number):
-    if number>=90:
-        print('A')
-    elif number >= 80:
-        print('B')
-    elif number >= 70:
-        print('C')
-    elif number >= 60:
-        print('D')
-    else:
-        print('F')
-
-while True:
-    number = int(input())
-    if 0<= number and number <= 100:
-        break
-
-compare_find(number)
+print("\    /\ ")
+print(" )  ( ')")
+print("(  /  )")
+print(" \(__)|")
