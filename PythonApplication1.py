27506f0d1a188db710c72b3e5999e313c2efc9e4
ErigoLee <57980181+ErigoLee@users.noreply.github.com>
2021-07-22 16:21:23 +0900

    2961

--- PythonApplication1.py
+++ PythonApplication1.py
@@ -1,25 +1,73 @@
-##funtion
-def sort_work(arr):
-    arr_len = len(arr)
-    i=0
-    while i < arr_len-1:
-        if arr[i]>arr[i+1]:
-            temp = arr[i]
-            arr[i] = arr[i+1]
-            arr[i+1] = temp
-            for j in arr:
-                print(j,end=" ")
-            print()
-        i=i+1
+N = 0  ##재료 갯수
+S = []  ##신맛
+B = []  ##쓴맛
+N_set = []
+min = 1000000000  ##최솟값
+include = []
 
-    index = 0
-    for i in range(0,arr_len-1,1):
-        if arr[i]>arr[i+1]:
-            index=1
 
-    if index==1:
-        sort_work(arr)
+##함수
+def powerSet(k):
+    T = []
+    if k == N:
+        for i in range(0, N, 1):
+            if (include[i]):
+                T.append(i)
+        N_set.append(T)
+        return
+    include[k] = False
+    powerSet(k + 1)
+    include[k] = True
+    powerSet(k + 1)
 
-##main
-arr = list(map(int,input().split()))
-sort_work(arr)
+
+##입력값
+
+while (True):
+    N = int(input())
+    if (0 <= N and N <= 10):
+        break
+
+j=0
+while (j<N):
+    total_s = 1
+    total_b = 0
+
+    s, b = map(int,input().split())
+
+    for i in S:
+        total_s = total_s * i
+    total_s = total_s * s
+    if total_s >= 1000000000:
+        continue
+
+    for i in B:
+        total_b = total_b + i
+    total_b = total_b + b
+    if total_b >= 1000000000:
+        continue
+
+    S.append(s)
+    B.append(b)
+    j = j+1
+
+for i in range(0, N, 1):
+    include.append(True)
+##여러 재료가 나오는 경우의 수
+powerSet(0)
+##신맛과 짠맛 차이 구하기
+T = len(N_set)
+for i in range(1, T, 1):
+    total_s = 1  ##신맛
+    total_b = 0  ##짠맛
+    diff = 0  ##차이값
+    for j in N_set[i]:
+        total_s = total_s * S[j]
+        total_b = total_b + B[j]
+    diff = abs(total_s - total_b)
+    if diff < min:
+        min = diff
+    if min == 0:
+        break
+
+print(min)
