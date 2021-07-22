17361ed03164053b1180403ee8a7057eee380a74
ErigoLee <57980181+ErigoLee@users.noreply.github.com>
2021-07-22 17:11:57 +0900

    9465

--- 소스.cpp
+++ 소스.cpp
@@ -1,45 +1,66 @@
 #include <iostream>
 using namespace std;
+int stickers[2][100001];
+int dp[2][100001];
 
-long long progress[101];
-long long results[101];
 
-
-long long counting(int n)
+int MAX(int a, int b)
 {
-    if (progress[n])
-        return progress[n];
-
-    if (n == 1 || n == 2 || n == 3)
-    {
-        progress[n] = 1;
-        return progress[n];
-    }
-
-    if (!progress[n - 3])
-        progress[n - 3] = counting(n - 3);
-
-    if (!progress[n - 2])
-        progress[n - 2] = counting(n - 2);
-
-    progress[n] = progress[n - 3] + progress[n - 2];
-
-    return progress[n];
+    if (a >= b)
+        return a;
+    else
+        return b;
 }
 
 int main() {
 
-    int test_case;
-    cin >> test_case;
+    int sticker_case;
+    int sum;
 
-    for (int i = 0; i < test_case; i++)
+    cin >> sticker_case;
+
+    for (int i = 0; i < sticker_case; i++)
     {
-        int n;
-        cin >> n;
-        results[i] = counting(n);
+
+        int N;
+        cin >> N;
+
+        for (int j = 0; j < 2; j++)
+        {
+            for (int k = 1; k <= N; k++)
+            {
+                int a;
+                cin >> a;
+                stickers[j][k] = a;
+            }
+        }
+        dp[0][0] = 0;
+        dp[1][0] = 0;
+        dp[0][1] = stickers[0][1];
+        dp[1][1] = stickers[1][1];
+
+
+        for (int j = 2; j <= N; j++)
+        {
+
+            dp[0][j] = MAX(dp[1][j - 1], dp[0][j - 2]) + stickers[0][j];
+            dp[0][j] = MAX(dp[0][j], dp[1][j - 2] + stickers[0][j]);
+
+            dp[1][j] = MAX(dp[0][j - 1], dp[0][j - 2]) + stickers[1][j];
+            dp[1][j] = MAX(dp[1][j], dp[1][j - 2] + stickers[1][j]);
+
+        }
+
+        if (dp[0][N] >= dp[1][N])
+            sum = dp[0][N];
+        else
+            sum = dp[1][N];
+
+        cout << sum << endl;
+        sum = 0;
+        for (int j = 1; j <= N; j++) dp[1][j] = dp[0][j] = 0;
+
     }
 
-    for (int i = 0; i < test_case; i++)
-        cout << results[i] << endl;
     return 0;
 }
