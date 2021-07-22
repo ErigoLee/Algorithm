d545171a843683742e7296cdab0513a1a5807f5c
ErigoLee <57980181+ErigoLee@users.noreply.github.com>
2021-07-22 16:26:28 +0900

    3020

--- 소스.cpp
+++ 소스.cpp
@@ -1,44 +1,65 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+
 using namespace std;
+int bottom[100001];
+int up[100001];
 
-int card_count[100];
-int main() {
-    int N, M;
-    int result = 0;
-    int gap = 100000;
-    cin >> N >> M;
-    vector<int>Com;
+//binary search
+int BSearch(int arr[], int len, int target) {
 
-    for (int i = 0; i < N; i++) {
-        cin >> card_count[i];
-        if (i < 3)
-            Com.push_back(1);
+    int first = 0;
+    int last = len - 1;
+    int mid;
+    int ans = 0;
+    while (first <= last)
+    {
+        mid = (first + last) / 2;
+        if (target > arr[mid]) {
+            ans = mid + 1;
+            first = mid + 1;
+        }
         else
-            Com.push_back(0);
+            last = mid - 1;
+
+    }
+    return ans;
+}
+
+
+int main() {
+
+    int count, level;
+    int min = 1000000000;
+    int cnt = 0;
+    cin >> count >> level;
+
+    for (int i = 0; i < count / 2; i++)
+    {
+        cin >> bottom[i] >> up[i];
     }
 
-    sort(Com.begin(), Com.end());
+    sort(bottom, bottom + count / 2);
+    sort(up, up + count / 2);
 
-    do {
-        int max = 0;
-        int gap2;
-        for (int i = 0; i < N; i++) {
-            if (Com[i] == 1)
-                max += card_count[i];
-        }
-        if (max <= M) {
-            gap2 = M - max;
-            if (gap2 < gap) {
-                result = max;
-                gap = gap2;
-            }
+
+    for (int i = 1; i <= level; i++)
+    {
+        int down_count = 0, up_count = 0;
+        int size = count / 2;
+        down_count = size - BSearch(bottom, size, i);
+        up_count = size - BSearch(up, size, level - i + 1);
+        if (min > down_count + up_count) {
+            min = down_count + up_count;
+            cnt = 0;
         }
 
-    } while (next_permutation(Com.begin(), Com.end()));
+        if (min == down_count + up_count)
+            cnt++;
+    }
 
-    cout << result << endl;
+    cout << min << " " << cnt;
 
     return 0;
 }
