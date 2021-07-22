5d063fc91a9c11bbe3580bb5dcea054eb305f23c
ErigoLee <57980181+ErigoLee@users.noreply.github.com>
2021-07-22 14:26:41 +0900

    1966

--- 소스.cpp
+++ 소스.cpp
@@ -1,54 +1,54 @@
 #include <iostream>
-#include <queue>
 #include <vector>
-#define N 100001
-#define INF 1000000007;
+#include <queue>
+#include <algorithm>
 
 using namespace std;
-vector<pair<int, int>> tree[N];
-
-long long result;
-long long dep(int here, int before)
-{
-    int size = tree[here].size();
-    long long sum = 1;
-    for (int i = 0; i < size; i++)
-    {
-        int next_node = tree[here][i].first;
-        int next_weight = tree[here][i].second;
-        if (next_node != before)
-        {
-            long long check = dep(next_node, here) * next_weight % INF;
-            result = (result + check * sum) % INF;
-            sum = (sum + check) % INF;
-        }
-    }
-    return sum;
-}
-
 
 int main() {
+    queue <int> q;
+    vector <int> priority;
+    vector <int> result;
+    int count;  //È½¼ö
+    int amount; //°³¼ö
+    int index;
+    cin >> count;
 
-    int node_count;
-    long long sum = 0;
-    cin >> node_count;
-    int a[N];
-    for (int i = 0; i < node_count - 1; i++)
-    {
-        int node_1, node_2, weight;
-        cin >> node_1 >> node_2 >> weight;
+    for (int i = 0; i < count; i++) {
+        cin >> amount >> index;
+        for (int j = 0; j < amount; j++) {
+            int a;
+            cin >> a;
+            q.push(a);
+            priority.push_back(a);
+        }
 
-        tree[node_1].push_back({ node_2,weight });
-        tree[node_2].push_back({ node_1,weight });
-        a[node_1]++;
-        a[node_2]++;
+        int count2 = 0;
+        sort(priority.begin(), priority.end());
+        while (true) {
+            if (q.front() == priority.back()) {
+                count2++;
+                if (index == 0) {
+                    result.push_back(count2);
+                    break;
+                }
+                q.pop();
+                priority.pop_back();
+            }
+            else {
+                int num = q.front();
+                q.pop();
+                q.push(num);
+            }
+            if (index == 0) index = q.size() - 1;
+            else index--;
+        }
+        while (!q.empty()) q.pop();
+        priority.clear();
     }
 
-
-
-    sum = dep(1, 0);
-
-    cout << result << endl;
+    for (int i = 0; i < count; i++)
+        cout << result[i] << endl;
 
     return 0;
 }
