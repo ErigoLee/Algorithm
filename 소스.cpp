e8eced8a2638abb9b04e631ac405f76138e51f75
ErigoLee <57980181+ErigoLee@users.noreply.github.com>
2021-07-22 16:10:21 +0900

    2748

--- 소스.cpp
+++ 소스.cpp
@@ -1,58 +1,16 @@
 #include <iostream>
+
 using namespace std;
-int storage[501][20]; // 메모화 재귀 표현
-int title(int N, int bits)
-{
-    int result = 0;
-    if (N <= 0) return 0;
-    if (N == 1 && bits == 6) return 0;
-    if (N == 1) return 1;
-    if (storage[N][bits]) return storage[N][bits];
-
-    switch (bits)
-    {
-    case 0:
-        result = title(N - 1, 0) + title(N - 1, 3) + title(N - 1, 9) + title(N - 1, 12) + title(N - 1, 15);
-        break;
-    case 3:
-        result = title(N - 1, 0) + title(N - 1, 12);
-        break;
-    case 6:
-        result = title(N - 1, 9);
-        break;
-    case 9:
-        result = title(N - 1, 0) + title(N - 1, 6);
-        break;
-    case 12:
-        result = title(N - 1, 0) + title(N - 1, 3);
-        break;
-    case 15:
-        result = title(N - 1, 0);
-        break;
-    }
-    storage[N][bits] = result;
-    return result;
-}
-
-
 int main() {
+    long long fibo[91];
+    int number;
+    cin >> number;
 
-    int testcase;
-    cin >> testcase;
+    fibo[0] = 0;
+    fibo[1] = 1;
+    for (int i = 2; i <= number; i++)
+        fibo[i] = fibo[i - 1] + fibo[i - 2];
 
-    int results[1001];
-
-    for (int i = 0; i < testcase; i++)
-    {
-        int N;
-        cin >> N;
-        results[i] = title(N, 0);
-    }
-
-    for (int i = 0; i < testcase; i++)
-    {
-        cout << results[i] << endl;
-    }
-
+    cout << fibo[number] << endl;
     return 0;
 }
