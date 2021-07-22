75c56a02e37410642fe75b26660c898c9e8215a3
ErigoLee <57980181+ErigoLee@users.noreply.github.com>
2021-07-22 16:51:38 +0900

    8393

--- PythonApplication1.py
+++ PythonApplication1.py
@@ -1,18 +1,9 @@
-##value
-count = list()
 ##main
-num = int(input())
-for i in range(0,num,1):
-    b = list(map(int,input().split()))
-    sum = 0
-    for j in range(1,len(b),1):
-        sum = sum +b[j]
-    averge = sum/b[0]
-    count_2 = 0
-    for j in range(1,len(b),1):
-        if b[j] > averge:
-            count_2 = count_2+1
-    temp = count_2/b[0] * 100
-    count.append(temp)
-for i in count:
-    print("%0.3f%%"%i)
+while True:
+    num = int(input())
+    if 1 <= num and num <=10000:
+        break
+sum = 0
+for i in range(1,num+1,1):
+    sum=sum+i
+print(sum)
