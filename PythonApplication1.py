620bac2595398b57891a406b087693b27c69999f
ErigoLee <57980181+ErigoLee@users.noreply.github.com>
2021-07-22 16:12:48 +0900

    2753

--- PythonApplication1.py
+++ PythonApplication1.py
@@ -1,3 +1,10 @@
-num = int(input())
-for i in range(num,0,-1):
-    print(i)
+##function
+def compare_find(number):
+    if number % 4 == 0 and (number % 100 !=0 or number % 400 == 0):
+        print(1)
+    else:
+        print(0)
+
+##main
+number = int(input())
+compare_find(number)
