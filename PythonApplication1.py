da9079f5168447bab3691df65957700e554501b5
ErigoLee <57980181+ErigoLee@users.noreply.github.com>
2021-07-22 17:53:57 +0900

    10974

--- PythonApplication1.py
+++ PythonApplication1.py
@@ -1,39 +1,21 @@
-##다음 순열을 찾을 수 있는 함수
-def next_permutation():
-    i = N-1
-    while i > 0 and T[i-1]>=T[i]:
-        i = i-1
-    if i == 0:
-        return False
-    j=N-1
-    while T[i-1] >= T[j]:
-        j = j - 1
-    T[i-1],T[j]=T[j],T[i-1]
-    j = N - 1
-    while i<j:
-        T[i],T[j] = T[j],T[i]
-        j = j-1
-        i = i+1
-    return True
+import math
+import itertools
+N = 0
+T = []
+s = []
+
 ##입력값 받기
 while(True):
     N = int(input())
-    if (1<=N and N<=10000):
+    if 1<=N and N<=8:
         break
 
-##순열값 받기
-while(True):
-    a = 0
-    input_number = map(int,input().split())
-    T = list(input_number)
-    for i in T:
-        if (i > N or i<=0):
-            a = a+1
-    if a == 0:
-        break
-S = next_permutation()
-if S:
-    for i in T:
-        print(i,end=" ")
-else:
-    print(-1)
+factor_num = math.factorial(N)
+
+for i in range(1,N+1,1):
+    T.append(i)
+s =itertools.permutations(T)
+for k in s:
+    for j in range(0,N,1):
+        print(k[j],end=" ")
+    print()
