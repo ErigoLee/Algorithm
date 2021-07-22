2e3811b1b49f254747c18d0fbd70d0ad44e2b8af
ErigoLee <57980181+ErigoLee@users.noreply.github.com>
2021-07-22 16:32:28 +0900

    4344

--- PythonApplication1.py
+++ PythonApplication1.py
@@ -1,13 +1,18 @@
-num = list()
+##value
 count = list()
 ##main
-for i in range(0,10,1):
-    input_num = int(input())
-    num.append(input_num)
-
-for i in range(0,10,1):
-    a = num[i] % 42
-    if a not in count:
-        count.append(a)
-
-print(len(count))
+num = int(input())
+for i in range(0,num,1):
+    b = list(map(int,input().split()))
+    sum = 0
+    for j in range(1,len(b),1):
+        sum = sum +b[j]
+    averge = sum/b[0]
+    count_2 = 0
+    for j in range(1,len(b),1):
+        if b[j] > averge:
+            count_2 = count_2+1
+    temp = count_2/b[0] * 100
+    count.append(temp)
+for i in count:
+    print("%0.3f%%"%i)
