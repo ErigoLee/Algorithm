9371b523d99cfdd9356ecd42987ce500ed1976eb
ErigoLee <57980181+ErigoLee@users.noreply.github.com>
2021-07-22 16:30:42 +0900

    3052

--- PythonApplication1.py
+++ PythonApplication1.py
@@ -1,32 +1,13 @@
-check_ring = list()
-##유클리드 호제법
-def fun1(i,j):
-    while i%j!=0:
-        k = i % j
-        i = j
-        j = k
-    return j
-
-def fun2(a,b):
-
-    if a >= b:
-        k = fun1(a,b)
-    else:
-        k = fun1(b,a)
-
-    a = a/k
-    b = b/k
-    check_ring.append((a,b))
-
+num = list()
+count = list()
 ##main
-num = int(input())
-ring = list(map(int,input().split()))
+for i in range(0,10,1):
+    input_num = int(input())
+    num.append(input_num)
 
-stand_ring = ring[0]
-for i in ring:
-    if i == stand_ring:
-        continue
-    fun2(stand_ring,i)
+for i in range(0,10,1):
+    a = num[i] % 42
+    if a not in count:
+        count.append(a)
 
-for j in check_ring:
-    print("%d/%d" %(j[0],j[1]))
+print(len(count))
