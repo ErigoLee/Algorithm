fbb095e73e551d653f265143030058866262e6dc
이다연 <hjkl311@naver.com>
2021-08-09 23:26:56 +0900

    2775

--- 소스.cpp
+++ 소스.cpp
@@ -1,35 +1,39 @@
 #include <iostream>
 #include <string>
+#include <vector>
 
 using namespace std;
-
+//k->Ãþ n->È£
+int home(int k, int n) {
+	if (k == 0) {
+		return n;
+	}
+	else {
+		int sum = 0;
+		for (int i = 1; i <= n; i++) {
+			sum += home(k-1,i);
+		}
+		return sum;
+	}
+}
 
 
 
 int main() {
 
-	int N;
-	cin >> N;
-	bool check = false;
-	
-	int num = N / 5;
-	for (int i = num; i > -1; i--) {
-		int temp = N - (i * 5);
-		int num2 = temp / 3;
-		int temp2 = temp % 3;
-		if (temp2 == 0)
-		{
-			check = true;
-			cout << (i + num2) << endl;
-			break;
-		}
-
-	}
-	if (!check) {
-		cout << -1 << endl;
+	int T;
+	cin >> T;
+	vector<int> answer;
+	for (int i = 0; i < T; i++) {
+		int k, n;
+		cin >> k >> n;
+		int an = home(k,n);
+		answer.push_back(an);
 	}
 
-
+	for (int i = 0; i < answer.size(); i++) {
+		cout << answer[i] << endl;
+	}
 
 	return 0;
 }
