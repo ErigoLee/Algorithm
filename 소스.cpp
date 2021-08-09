e2019f1a8a83723c0c4cb741ca7e2be05031ed26
이다연 <hjkl311@naver.com>
2021-08-10 00:05:52 +0900

    1193

--- 소스.cpp
+++ 소스.cpp
@@ -3,37 +3,47 @@
 #include <vector>
 
 using namespace std;
-//k->Ãþ n->È£
-int home(int k, int n) {
-	if (k == 0) {
-		return n;
-	}
-	else {
-		int sum = 0;
-		for (int i = 1; i <= n; i++) {
-			sum += home(k-1,i);
-		}
-		return sum;
-	}
-}
+
 
 
 
 int main() {
 
-	int T;
-	cin >> T;
-	vector<int> answer;
-	for (int i = 0; i < T; i++) {
-		int k, n;
-		cin >> k >> n;
-		int an = home(k,n);
-		answer.push_back(an);
-	}
+	int num;
+	int count = 0;
+	cin >> num;
 
-	for (int i = 0; i < answer.size(); i++) {
-		cout << answer[i] << endl;
+	int check, check2;
+	bool pass = false;
+
+	int num2 = 0;
+	while (true) {
+		num2++;
+		for (int i = 0; i <num2; i++) {
+			count++;
+			if (count == num) {
+
+				if (num2 % 2 == 1) {
+					check2 = i + 1;
+					check = num2 - (i);
+				}
+				else {
+					check = i + 1;
+					check2 = num2 - (i);
+				}
+				
+				pass = true;
+				break;
+			}
+		}
+		if (pass)
+			break;
+		
 	}
+	
+	string answer = to_string(check) + "/" + to_string(check2);
+
+	cout << answer << endl;
 
 	return 0;
 }
