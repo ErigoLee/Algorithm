e2b1e987e8e380a1e1984eb32c6ea9652e075314
이다연 <hjkl311@naver.com>
2021-08-10 22:52:34 +0900

    10250

--- 소스.cpp
+++ 소스.cpp
@@ -5,32 +5,40 @@
 using namespace std;
 
 int main() {
-	int A, B, V;
-	cin >> A >> B >> V;
-	int temp = V;
-	temp = temp - A;
-	int count = temp / (A - B);
-	if (count * (A - B) + A >= V) {
-		cout << count + 1 << endl;
-	}
-	else {
-		cout << count + 2 << endl;
-	}
-
-	/*
-	int count = 0;
-	int sum = 0;
-	while (true) {
-		sum += A;
-		count++;
-		if (sum >= V) {
-			break;
-		}
-		sum -= B;
-	}
-	cout << count << endl;
-	*/
+	int testcase;
+	int H, W, N;
+	cin >> testcase;
 	
+	
+	vector<int> answer;
 
+	for (int k = 0; k < testcase; k++) {
+		int count = 0;
+		cin >> H >> W >> N;
+		bool check = false;
+		int answer_H = 0;
+		int answer_W = 0;
+		for (int i = 1; i <= W; i++) {
+			for (int j = 1; j <= H; j++) {
+				count++;
+				if (count == N)
+				{
+					answer_W = i;
+					answer_H = j;
+					check = true;
+					break;
+				}
+			}
+			if (check) {
+				break;
+			}
+		}
+
+		answer.push_back(answer_H*100+answer_W);
+	}
+
+	for (int i = 0; i < answer.size(); i++) {
+		cout << answer[i] << endl;
+	}
 	return 0;
 }
