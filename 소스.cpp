32441dc24be5bb6c715a53b9ddaf678ba6fdd839
이다연 <hjkl311@naver.com>
2021-08-10 11:19:57 +0900

    2869

--- 소스.cpp
+++ 소스.cpp
@@ -4,46 +4,33 @@
 
 using namespace std;
 
-
-
-
 int main() {
-
-	int num;
-	int count = 0;
-	cin >> num;
-
-	int check, check2;
-	bool pass = false;
-
-	int num2 = 0;
-	while (true) {
-		num2++;
-		for (int i = 0; i <num2; i++) {
-			count++;
-			if (count == num) {
-
-				if (num2 % 2 == 1) {
-					check2 = i + 1;
-					check = num2 - (i);
-				}
-				else {
-					check = i + 1;
-					check2 = num2 - (i);
-				}
-				
-				pass = true;
-				break;
-			}
-		}
-		if (pass)
-			break;
-		
+	int A, B, V;
+	cin >> A >> B >> V;
+	int temp = V;
+	temp = temp - A;
+	int count = temp / (A - B);
+	if (count * (A - B) + A >= V) {
+		cout << count + 1 << endl;
 	}
-	
-	string answer = to_string(check) + "/" + to_string(check2);
+	else {
+		cout << count + 2 << endl;
+	}
 
-	cout << answer << endl;
+	/*
+	int count = 0;
+	int sum = 0;
+	while (true) {
+		sum += A;
+		count++;
+		if (sum >= V) {
+			break;
+		}
+		sum -= B;
+	}
+	cout << count << endl;
+	*/
+	
 
 	return 0;
 }
