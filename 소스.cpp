1f76f2d7b7a823af648db0175c2bf040e1e5685d
이다연 <hjkl311@naver.com>
2021-08-11 22:20:17 +0900

    4673

--- 소스.cpp
+++ 소스.cpp
@@ -4,27 +4,64 @@
 
 using namespace std;
 
-int main() {
+bool checking(int checkNum, int Num) {
 
-	int x, y;
-	cin >> x;
-	cin >> y;
+	if (checkNum >= 1000) {
+		int n_1000 = checkNum / 1000;
+		int n_100 = (checkNum - n_1000 * 1000) / 100;
+		int n_10 = (checkNum - n_1000 * 1000 - n_100 * 100) / 10;
+		int n_1 = (checkNum - n_1000 * 1000 - n_100 * 100) % 10;
 
-	if (x > 0) {
-		if (y > 0) {
-			cout << 1 << endl;
-		}
-		else {
-			cout << 4 << endl;
-		}
+		if (checkNum + n_1000 + n_100 + n_10 + n_1 == Num)
+			return true;
+		else
+			return false;
+	}
+	else if (checkNum >= 100) {
+		int n_100 = checkNum / 100;
+		int n_10 = (checkNum - n_100 * 100) / 10;
+		int n_1 = (checkNum - n_100 * 100) % 10;
+		if (checkNum + n_100 + n_10 + n_1 == Num)
+			return true;
+		else
+			return false;
+	}
+	else if (checkNum >= 10) {
+		int n_10 = checkNum / 10;
+		int n_1 = checkNum % 10;
+		if (checkNum + n_10 + n_1 == Num)
+			return true;
+		else
+			return false;
 	}
 	else {
-		if (y > 0) {
-			cout << 2 << endl;
+		if (checkNum + checkNum == Num)
+			return true;
+		else
+			return false;
+	}
+}
+
+
+int main() {
+
+	vector<int> num;
+	
+	
+	for (int i = 1; i < 10000; i++) {
+		bool check = false;
+		for (int j = 1; j <= i; j++) {
+			check = checking(j, i);
+			if (check)
+				break;
 		}
-		else {
-			cout << 3 << endl;
+		if (!check) {
+			num.push_back(i);
 		}
+	}
+
+	for (int i = 0; i < num.size(); i++) {
+		cout << num[i] << endl;
 	}
 	
 	return 0;
