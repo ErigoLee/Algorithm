a23b659b1c8dc5643245731da2ed22ff5d32b4f9
이다연 <hjkl311@naver.com>
2021-08-11 21:52:59 +0900

    14681

--- 소스.cpp
+++ 소스.cpp
@@ -6,60 +6,25 @@ using namespace std;
 
 int main() {
 
-	int N;
-	cin >> N;
+	int x, y;
+	cin >> x;
+	cin >> y;
 
-	vector<vector<string>> star;
-
-	bool check = false;
-	for (int i = 0; i < N * 2; i++) {
-		vector<string> temp;
-		if (i % 2 == 0)
-		{
-			check = true;
+	if (x > 0) {
+		if (y > 0) {
+			cout << 1 << endl;
 		}
 		else {
-			check = false;
+			cout << 4 << endl;
 		}
-		for (int j = 0; j < N; j++) {
-			if (check) {
-				if (j % 2 == 0) {
-					string a = "*";
-					temp.push_back(a);
-				}
-				else {
-					string a = " ";
-					temp.push_back(a);
-				}
-			}
-			else {
-				if (j % 2 == 0) {
-					string a = " ";
-					temp.push_back(a);
-				}
-				else {
-					string a = "*";
-					temp.push_back(a);
-				}
-			}
-		}
-		star.push_back(temp);
-		temp.clear();
 	}
-
-	if (N == 1) {
-		star.clear();
-		vector<string> temp;
-		temp.push_back("*");
-		star.push_back(temp);
-	}
-
-
-	for (int i = 0; i < star.size(); i++) {
-		for (int j = 0; j < star[0].size(); j++) {
-			cout << star[i][j];
+	else {
+		if (y > 0) {
+			cout << 2 << endl;
 		}
-		cout << endl;
+		else {
+			cout << 3 << endl;
+		}
 	}
 	
 	return 0;
