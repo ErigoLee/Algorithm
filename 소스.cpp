507cbad068ed63426c542f6213f694f595899c4e
이다연 <hjkl311@naver.com>
2021-08-12 01:16:16 +0900

    2941

--- 소스.cpp
+++ 소스.cpp
@@ -4,37 +4,68 @@
 
 using namespace std;
 
-int DaieolNum(char daieolChar) {
-
-	if (daieolChar >= 'A' && daieolChar <= 'C')
-		return 3;
-	else if (daieolChar >= 'D' && daieolChar <= 'F')
-		return 4;
-	else if (daieolChar >= 'G' && daieolChar <= 'I')
-		return 5;
-	else if (daieolChar >= 'J' && daieolChar <= 'L')
-		return 6;
-	else if (daieolChar >= 'M' && daieolChar <= 'O')
-		return 7;
-	else if (daieolChar >= 'P' && daieolChar <= 'S')
-		return 8;
-	else if (daieolChar >= 'T' && daieolChar <= 'V')
-		return 9;
-	else
-		return 10;
-
-}
 
 int main() {
 
 	string input_str;
 	cin >> input_str;
-	int sum = 0;
+	
+	int count = 0;
 	for (int i = 0; i < input_str.size(); i++) {
-		sum += DaieolNum(input_str[i]);
+		count++;
+		if (input_str[i] == 'c') {
+			if (i + 1 < input_str.size()) {
+				if (input_str[i + 1] == '=')
+					i = i + 1;
+				if (input_str[i + 1] == '-')
+					i = i + 1;
+			}
+		}
+
+		if (input_str[i] == 'd') {
+			if (i + 1 < input_str.size()) {
+				if (input_str[i + 1] == 'z') {
+					if (i + 2 < input_str.size()) {
+						if (input_str[i + 2] == '=')
+							i = i + 2;
+					}
+				}
+				if (input_str[i + 1] == '-')
+					i = i + 1;
+			}
+		}
+
+		if (input_str[i] == 'l') {
+			if (i + 1 < input_str.size()) {
+				if (input_str[i + 1] == 'j')
+					i = i + 1;
+			}
+		}
+
+		if (input_str[i] == 'n'){
+			if (i + 1 < input_str.size()) {
+				if (input_str[i + 1] == 'j')
+					i = i + 1;
+			}
+		}
+
+		if (input_str[i] == 's') {
+			if (i + 1 < input_str.size()) {
+				if (input_str[i + 1] == '=')
+					i = i + 1;
+			}
+		}
+		
+		if (input_str[i] == 'z') {
+			if (i + 1 < input_str.size()) {
+				if (input_str[i + 1] == '=')
+					i = i + 1;
+			}
+		}
+		
 	}
-	
-	cout << sum << endl;
-	
+
+	cout << count << endl;
+
 	return 0;
 }
