e0a876064e19a088edf0a9659d94976ef0477b33
DianaLee <yirigo2017@gmail.com>
2022-03-13 19:30:35 +0900

    2525

--- Project10/소스.cpp
+++ Project10/소스.cpp
@@ -3,7 +3,19 @@
 using namespace std;
 
 int main() {
-	int year;
-	cin >> year;
-	cout << year - 543 << endl;
+	int hour; int min; //입력시간
+	int interval; //요리하는데 필요한 시간
+	cin >> hour >> min;
+	cin >> interval;
+	min = min + interval;
+	if (min >= 60) {
+		int temp_hour = min / 60;
+		int temp_min = min % 60;
+		hour += temp_hour;
+		min = temp_min;
+	}
+	if (hour >= 24) {
+		hour = hour % 24;
+	}
+	cout << hour << " " << min << endl;
 }
