7e3a4774c5f568ef7ce6863e11462758cdfeb8b3
DianaLee <yirigo2017@gmail.com>
2022-03-15 17:56:42 +0900

    1929

--- Project11/소스.cpp
+++ Project11/소스.cpp
@@ -1,18 +1,32 @@
 #include <iostream>
-
+#include <cmath>
 using namespace std;
 
 int main() {
-	int N;
-	cin >> N;
-	while (N > 1) {
-		for (int i = 2; i <= 10000000; i++) {
-			if (N % i == 0) {
-				cout << i << endl;
-				N = N / i;
-				break;
+	int start, end;
+	//입출력 시간 단축시키기
+	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+	cin >> start >> end;
+	bool* numbers = new bool[end + 1];
+	numbers[0] = false;
+	numbers[1] = false;
+	for (int i = 2; i <= end; i++)
+		numbers[i] = true;
+	//에라토스테네스의 체
+	for (int i = 2; i <= sqrt(end); i++) {
+		if (numbers[i] == false) {
+			continue;
+		}
+		else {
+			for (int j = i * i; j <= end; j = j + i) {
+				if (numbers[j] == true)
+					numbers[j] = false;
 			}
 		}
 	}
-	return 0;
+	for (int i = start; i <= end; i++) {
+		if (numbers[i] == true)
+			cout << i << "\n";
+	}
+	
 }
